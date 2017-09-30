af869218a95d381a6242a05eabaeb0be81f91911
brianchristopherpeck <brianchristopherpeck@gmail.com>
2017-09-30 01:05:55 -0700

    Added stateless functional components and advanced es6 destructuring

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,16 +1,12 @@
 var React = require('react');
 
-// Presentational component... takes props and renders to screen
-var WeatherMessage = React.createClass({
-  render: function() {
-    var {temp, location} = this.props;
-
-    return (
-      <div>
-        <p>It's {temp} in {location}</p>
-      </div>
-    );
-  }
-});
+// Stateless functional components with advanced ES6 destructuring
+var WeatherMessage = ({temp, location}) => { // temp and location are props
+  return (
+    <div>
+      <p>It's {temp} in {location}</p>
+    </div>
+  );
+}
 
 module.exports = WeatherMessage;
