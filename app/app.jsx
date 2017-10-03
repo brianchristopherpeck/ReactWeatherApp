b54429ea2c42dd9ae15f3ad307b38cb234776067
brianchristopherpeck <brianchristopherpeck@gmail.com>
2017-10-03 02:40:35 -0700

    Edited package.json and Webpack.config for Foundation Styles

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,12 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+// Initialize foundation
+$(document).foundation();
+
 
 // Prop variables. Get passed to component on initialization
 ReactDOM.render(
