8ca1a235470c23df36162b059a2b86fa9f75a6cf
brianchristopherpeck <brianchristopherpeck@gmail.com>
2017-10-05 02:51:19 -0700

    Style Nav Bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,15 +2,53 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 // Stateless functional component
-var Nav = (props) => {
-	return (
-		<div>
-			<h3>Nav Component</h3>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-		</div>
-	);
-}
+var Nav = React.createClass({
+	onSearch: function (e) {
+		e.preventDefault();
+		alert('Not yet wired');
+	},
+	render: function () {
+		return (
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">React Weather App</li>
+						<li>
+							<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+						</li>
+						<li>
+							<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+						</li>
+						<li>
+							<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<form onSubmit={this.onSearch}>
+						<ul className="menu">
+							<li>
+								<input type="search" placeholder="Search Weather"/>
+							</li>
+							<li>
+								<input type="submit" className="button" value="Get Weather"/>
+							</li>
+						</ul>
+					</form>
+				</div>
+			</div>
+		);
+	}
+})
 
 module.exports = Nav;
+
+
+var old = (
+	<div>
+		<h3>Nav Component</h3>
+		<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+		<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+		<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+	</div>
+);
